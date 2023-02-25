d07acb72e6b1ee915ee54c95e432c1b4bfdab1cf
Marc Girbau <marc.girbau94@gmail.com>
2023-02-25 13:29:27 +0100

    Create LoginForm component

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import App from "./App";
-import "./index.css";
+import App from "./components/App/App";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Provider store={store}>
       <App />
     </Provider>
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+*, ::after, ::before {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 1rem;
+  background-color: #fff;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  width: 100%;
+  min-height: 100vh;
+  
+}
+
+h1,h2{
+  padding: 0;
+  margin: 0;
+}
+
+ol, ul, li {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
+button{
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+}
+`;
+
+export default GlobalStyles;
