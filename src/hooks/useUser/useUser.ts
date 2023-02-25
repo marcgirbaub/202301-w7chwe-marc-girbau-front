3ccc45aad03e6a377913ce826f50b3f2d40b15bc
Marc Girbau <marc.girbau94@gmail.com>
2023-02-25 12:35:15 +0100

    Create useUser custom hook

---
+++ src/hooks/useUser/useUser.ts
@@ -0,0 +1,57 @@
+import decodeToken from "jwt-decode";
+import { User } from "../../store/features/userSlice/types";
+import {
+  loginUserActionCreator,
+  logoutUserActionCreator,
+} from "../../store/features/userSlice/userSlice";
+import { useAppDispatch } from "../../store/hooks";
+import { CustomTokenPayload, LoginResponse, UserCredentials } from "./types";
+
+interface UseUserStructure {
+  loginUser: (userCredentials: UserCredentials) => Promise<void>;
+  logoutUser: () => void;
+}
+
+const useUser = (): UseUserStructure => {
+  const dispatch = useAppDispatch();
+
+  const apiUrl = process.env.REACT_APP_URL_API;
+  const usersEndpoint = "/users";
+  const loginEndpoint = "/login";
+
+  const loginUser = async (userCredentials: UserCredentials) => {
+    try {
+      const response = await fetch(
+        `${apiUrl}${usersEndpoint}${loginEndpoint}`,
+        {
+          method: "POST",
+          body: JSON.stringify(userCredentials),
+          headers: { "Content-type": "application/json" },
+        }
+      );
+
+      const { token } = (await response.json()) as LoginResponse;
+
+      const tokenPayload: CustomTokenPayload = decodeToken(token);
+
+      const { sub: id, username } = tokenPayload;
+
+      const logginUser: User = {
+        username,
+        token,
+        id,
+      };
+
+      dispatch(loginUserActionCreator(logginUser));
+      localStorage.setItem("token", token);
+    } catch (error) {}
+  };
+
+  const logoutUser = () => {
+    dispatch(logoutUserActionCreator());
+  };
+
+  return { loginUser, logoutUser };
+};
+
+export default useUser;
