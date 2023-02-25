fa0c3b6f000b79e8433b58bdf0520354ccfce1a4
Marc Girbau <marc.girbau94@gmail.com>
2023-02-25 17:52:10 +0100

    Create routes and router provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { RouterProvider } from "react-router-dom";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import App from "./components/App/App";
 import GlobalStyles from "./styles/GlobalStyles";
+import { router } from "./routes/routes";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -13,7 +14,7 @@ root.render(
   <React.StrictMode>
     <GlobalStyles />
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
