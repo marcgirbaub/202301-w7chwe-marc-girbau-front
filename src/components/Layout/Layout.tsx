1e0e8d18d4e0edc3095658aefe083cd6f05dc6b4
Marc Girbau <marc.girbau94@gmail.com>
2023-02-26 14:17:43 +0100

    Test LoginForm functionality

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,21 +3,38 @@ import { Outlet, Link } from "react-router-dom";
 import LayoutStyled from "./LayoutStyled";
 import Loader from "../Loader/Loader";
 import { useAppSelector } from "../../store/hooks";
+import useUser from "../../hooks/useUser/useUser";
 
 const Layout = (): JSX.Element => {
   const { isLoading } = useAppSelector((state) => state.ui);
+  const { isLogged } = useAppSelector((state) => state.user);
+
+  const { logoutUser } = useUser();
 
   return (
     <LayoutStyled>
       <header className="main-header">
         <h1 className="main-header__title">SocialBall</h1>
         <nav className="main-header__nav nav">
-          <Link to={"login"} className="nav__login">
-            Log in
-          </Link>
-          <Link to={"register"} className="nav__signup">
-            Sign up
-          </Link>
+          {!isLogged && (
+            <Link to={"login"} className="nav__login">
+              Log in
+            </Link>
+          )}
+          {!isLogged && (
+            <Link
+              to={"register"}
+              className="btn btn-success nav__signup"
+              type="button"
+            >
+              Sign up
+            </Link>
+          )}
+          {isLogged && (
+            <button className="btn btn-danger nav__logout" onClick={logoutUser}>
+              Log out
+            </button>
+          )}
         </nav>
       </header>
       <ToastContainer hideProgressBar />
--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import Wrapper from "../../mocks/Wrapper";
 import LoginForm from "./LoginForm";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a LoginForm component", () => {
   describe("When it is rendered", () => {
@@ -38,6 +39,48 @@ describe("Given a LoginForm component", () => {
       });
 
       expect(expectedButtonText).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes on the username input", () => {
+    test("Then it change the value of this input", async () => {
+      const usernameLabel = "Username";
+      const usernameWritten = "ronaldinho";
+
+      render(
+        <Wrapper>
+          <LoginForm />
+        </Wrapper>,
+        { wrapper: BrowserRouter }
+      );
+
+      const usernameInput = screen.getByLabelText(usernameLabel);
+      await act(
+        async () => await userEvent.type(usernameInput, usernameWritten)
+      );
+
+      expect(usernameInput).toHaveValue(usernameWritten);
+    });
+  });
+
+  describe("When the user writes on the password input", () => {
+    test("Then it change the value of this input", async () => {
+      const passwordLabel = "Password";
+      const passwordWritten = "ronaldinho10";
+
+      render(
+        <Wrapper>
+          <LoginForm />
+        </Wrapper>,
+        { wrapper: BrowserRouter }
+      );
+
+      const passwordInput = screen.getByLabelText(passwordLabel);
+      await act(
+        async () => await userEvent.type(passwordInput, passwordWritten)
+      );
+
+      expect(passwordInput).toHaveValue(passwordWritten);
     });
   });
 });
